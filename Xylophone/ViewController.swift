b9bb4b3aeb0035ba6c9bf7ff038651cd76d9b7ff
Faiza Nawaz <Faizanawaz755@gmail.com>
2023-03-11 21:54:37 +0500

    practicing AVPlayer for audios

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     }
 
     
-    @IBAction func keyPressed(_ sender: UIButton) {        
+    @IBAction func keyPressed(_ sender: UIButton) {
         playSound(keyName: sender.titleLabel!.text!)
     }
     
