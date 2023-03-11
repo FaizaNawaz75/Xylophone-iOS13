21fcc7e8ee6df910a16be889d9167e6c0a4dcb2c
Faiza Nawaz <Faizanawaz755@gmail.com>
2023-03-11 21:49:44 +0500

    practicing AVPlayer for audios

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {        
+        playSound(keyName: sender.titleLabel!.text!)
+    }
+    
+    func playSound(keyName: String) {
+        
+        let url = Bundle.main.url(forResource: keyName, withExtension:"wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player!.play()
+    }
+    
 }
+
 
